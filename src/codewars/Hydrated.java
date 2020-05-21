831dfaf45b021511c8c1a1ef4498bb82cd19bd64
Victor <nikolaev.vikt@gmail.com>
2020-05-21 17:59:03 +0300

    [ADD] Hydrated class

---
+++ src/codewars/Hydrated.java
@@ -0,0 +1,11 @@
+package codewars;
+
+public class Hydrated {
+    public static int Liters(double time) {
+        return (int) (time * 0.5);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Liters(6.7));
+    }
+}
